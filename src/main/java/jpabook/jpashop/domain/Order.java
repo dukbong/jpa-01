e7e17a011910b90fc4192b92943c0e131142089b
jang <gorae930301@gmail.com>
2024-01-14 21:11:53 +0900

    연관관계 예제

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,81 @@
+package jpabook.jpashop.domain;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ORDERS")
+public class Order {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "ORDER_ID")
+	private Long id;
+	
+//	@Column(name = "MEMBER_ID")
+//	private Long memberId;
+
+	@ManyToOne
+	@JoinColumn(name = "MEMBER_ID")
+	private Member member;
+	
+	private LocalDateTime orderDate;
+	
+	@Enumerated(EnumType.STRING)
+	private OrderStatus status;
+	
+	@OneToMany(mappedBy = "order")
+	private List<OrderItem> orderItems = new ArrayList<>();
+	
+	
+	public void addOrderItem(OrderItem orderItem) {
+		orderItems.add(orderItem);
+		orderItem.setOrder(this);
+	}
+	
+	public Member getMember() {
+		return member;
+	}
+	public void setMember(Member member) {
+		this.member = member;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+//	public Long getMemberId() {
+//		return memberId;
+//	}
+//	public void setMemberId(Long memberId) {
+//		this.memberId = memberId;
+//	}
+	public LocalDateTime getOrderDate() {
+		return orderDate;
+	}
+	public void setOrderDate(LocalDateTime orderDate) {
+		this.orderDate = orderDate;
+	}
+	public OrderStatus getStatus() {
+		return status;
+	}
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+	
+	
+}
