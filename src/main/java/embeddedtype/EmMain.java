70ef454833faa11e2c0c92e4e4497abaf0893e58
jang <gorae930301@gmail.com>
2024-01-24 22:20:31 +0900

    값 타입과 불변 객체

--- src/main/java/embeddedtype/EmMain.java
+++ src/main/java/embeddedtype/EmMain.java
@@ -15,11 +15,29 @@ public class EmMain {
 		
 		tx.begin();
 		try {
-			EmMember member = new EmMember();
-			member.setUsername("HELLO");
-			member.setHomeAddress(new Address("city", "street","zipcode"));
-			member.setWorkPeriod(new Period(LocalDateTime.now(), LocalDateTime.now()));
-			em.persist(member);
+			Address address = new Address("city", "street", "10000");
+			
+			EmMember member1 = new EmMember();
+			member1.setUsername("HELLO");
+			member1.setHomeAddress(address);
+//			member1.setWorkPeriod(new Period(LocalDateTime.now(), LocalDateTime.now()));
+			em.persist(member1);
+
+			Address newAddress = new Address("Newcity", address.getStreet(), address.getZipcode());
+			member1.setHomeAddress(newAddress);
+			
+			// 아래의 부작용을 없애기 위해서는
+//			Address copyAddress = new Address(address.getCity(), address.getStreet(), address.getZipcode());
+			// 이런 방법 혹은 setter를 없애버리거나 private으로 만들면 된다.
+//			EmMember member2 = new EmMember();
+//			member2.setUsername("HELLO");
+//			member2.setHomeAddress(copyAddress);
+//			em.persist(member2);
+			
+			// 임베디드 타입 값을 여러 엔티티에서 공유하면 위험한 이유
+			// 이러면 update 쿼리가 member1, member2 모두 날아간다
+			// 부작용!!
+//			member1.getHomeAddress().setCity("newCity");
 			
 			tx.commit();
 		}catch (Exception e) {
