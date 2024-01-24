dc38aaf80a454f1368c1a7a6025f045d98a8280f
jang <gorae930301@gmail.com>
2024-01-24 23:15:40 +0900

    값타입 예제

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -18,9 +19,13 @@ public class Member extends BaseEntity {
 	@Column(name = "MEMBER_ID")
 	private Long id;
 	private String name;
-	private String city;
-	private String street;
-	private String zipcode;
+	
+//	값타입으로 대체
+//	private String city;
+//	private String street;
+//	private String zipcode;
+	@Embedded
+	private Address address;
 	
 	@OneToMany(mappedBy = "member")
 	private List<Order> orders = new ArrayList<>();
@@ -37,25 +42,18 @@ public class Member extends BaseEntity {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getCity() {
-		return city;
+	public Address getAddress() {
+		return address;
 	}
-	public void setCity(String city) {
-		this.city = city;
+	public void setAddress(Address address) {
+		this.address = address;
 	}
-	public String getStreet() {
-		return street;
+	public List<Order> getOrders() {
+		return orders;
 	}
-	public void setStreet(String street) {
-		this.street = street;
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
 	}
-	public String getZipcode() {
-		return zipcode;
-	}
-	public void setZipcode(String zipcode) {
-		this.zipcode = zipcode;
-	}
-	
 	
 	
 }
